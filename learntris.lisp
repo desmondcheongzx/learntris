bf611cf164f1b5129b1628453403960a8725991f
Desmond Cheong <desmondcheongzx@gmail.com>
2016-11-12 13:44:47 +0800

    Added initialisation and board printer

--- learntris.lisp
+++ learntris.lisp
@@ -1,3 +1,26 @@
 #!/usr/bin/sbcl --script
 
+(defvar *matrix* nil)
+
+(defun empty-board ()
+  (loop for i from 1 to 22
+     collect (loop for j from 1 to 10 collect '\.)))
+
+(defun init ()
+  (setf *matrix* (empty-board)))
+
+(defun game ()
+  (loop for input = (read)
+     until (equal input 'q)
+     do (input-handler input)))
+
+(defun input-handler (input)
+  (case input
+    (p (print-board))))
+
+(defun print-board ()
+  (format t "~{~{~a~^ ~}~%~}" *matrix*))
+
+(init)
+(game)
 
