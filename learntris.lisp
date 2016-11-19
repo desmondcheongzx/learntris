6289667707efd5290a38687592547e4b9f8e5a74
Desmond Cheong <desmondcheongzx@gmail.com>
2016-11-19 23:25:16 +0800

    Added collisions and start menu. Some menu plumbing bugs introduced

--- learntris.lisp
+++ learntris.lisp
@@ -7,27 +7,30 @@
 (defvar empty-row nil)
 (defvar empty-board nil)			      
 (defvar *active-tetramino* nil)
-(defconstant i-tetramino '((#\. #\. #\. #\.)
-			   (#\c #\c #\c #\c)
-			   (#\. #\. #\. #\.)
-			   (#\. #\. #\. #\.)))
-(defconstant o-tetramino '((#\y #\y)
-			   (#\y #\y)))
-(defconstant z-tetramino '((#\r #\r #\.)
-			   (#\. #\r #\r)
-			   (#\. #\. #\.)))
-(defconstant s-tetramino '((#\. #\g #\g)
-			   (#\g #\g #\.)
-			   (#\. #\. #\.)))
-(defconstant j-tetramino '((#\b #\. #\.)
-			   (#\b #\b #\b)
-			   (#\. #\. #\.)))
-(defconstant l-tetramino '((#\. #\. #\o)
-			   (#\o #\o #\o)
-			   (#\. #\. #\.)))
-(defconstant t-tetramino '((#\. #\m #\.)
-			   (#\m #\m #\m)
-			   (#\. #\. #\.)))
+(defparameter i-tetramino '((#\. #\. #\. #\.)
+			    (#\c #\c #\c #\c)
+			    (#\. #\. #\. #\.)
+			    (#\. #\. #\. #\.)))
+(defparameter o-tetramino '((#\y #\y)
+			    (#\y #\y)))
+(defparameter z-tetramino '((#\r #\r #\.)
+			    (#\. #\r #\r)
+			    (#\. #\. #\.)))
+(defparameter s-tetramino '((#\. #\g #\g)
+			    (#\g #\g #\.)
+			    (#\. #\. #\.)))
+(defparameter j-tetramino '((#\b #\. #\.)
+			    (#\b #\b #\b)
+			    (#\. #\. #\.)))
+(defparameter l-tetramino '((#\. #\. #\o)
+			    (#\o #\o #\o)
+			    (#\. #\. #\.)))
+(defparameter t-tetramino '((#\. #\m #\.)
+			    (#\m #\m #\m)
+			    (#\. #\. #\.)))
+(defparameter welcome-message
+  "Learntris (c) 1992 Tetraminex, Inc.
+Press start button to begin.")
 
 (defun global-init ()
   (setf empty-row (loop for i from 1 to 10 collect #\.))
@@ -35,12 +38,15 @@
   (init))
 
 (defun init ()
+  (setf *handler* #'game-handler)
   (setf *lines-cleared* 0)
   (setf *score* 0)
   (setf *board* empty-board)
   (setf *tetramino-board* empty-board))
 
 (defvar end-game nil)
+(defvar *handler* nil)
+
 (defun game ()
   (global-init)
   (block quit
@@ -59,11 +65,17 @@
 	 when (eql c #\?)
 	 do (setf special t)
 	 else unless (eql c #\space)
-	 do (progn (input-handler (if special (specify c) c))
+	 do (progn (funcall *handler* (if special (specify c) c))
 		   (setf special nil))))))
 
-(defun input-handler (input)
+(defun menu-handler (input)
   (case input
+    (#\p (display welcome-message))
+    (#\! (setf *handler* #'game-handler))))
+
+(defun game-handler (input)
+  (case input
+    (#\@ (setf *handler* #'menu-handler))
     (#\p (print-board))
     (#\q (funcall end-game))
     (#\g (input-board))
@@ -76,21 +88,23 @@
     (#\) (rotate-clockwise))
     (#\( (rotate-anti-clockwise))
     (#\; (format t "~%"))
-    (#\P (print-matrix *tetramino-board*))
+    (#\P (print-board :active t))
     ((#\< #\> #\v) (nudge input))
     (#\V (plunge))))
 
 (defun next-step ()
-  (setf *board*
-	(loop for row in *board*
-	   collect (if (unbroken-row-p row)
-		       (progn (incf *lines-cleared*)
-			      (incf *score* 100)
-			      empty-row)
-		       row))))
+  (loop for row in *board*
+     for x = 0 then (1+ x)
+     when (unbroken-row-p row)
+     do (progn (incf *lines-cleared*)
+	       (incf *score* 100)
+	       (setf (elt *board* x) empty-row))))
+	   
+(defun blank-p (char)
+  (eql char #\.))
 
 (defun unbroken-row-p (row)
-  (loop for i in row never (char= i #\.)))
+  (loop for x in row never (blank-p x)))
 
 (defun insert-tetramino ()
   (loop initially (setf *tetramino-board* (copy-list empty-board))
@@ -106,24 +120,32 @@
 	      (progn (push #\. row) (setf right t)))))
   row)
 
+(defun clash-p (board)
+  (loop for ya in *board*
+     for yb in board
+     thereis (not (loop for xa in ya
+		     for xb in yb
+		     always (or (blank-p xb) (blank-p xa))))))
+
 (defun collision-p (direction)
   (case direction
     (#\< (notevery #'(lambda (row) (eql #\. (car row)))
-		*tetramino-board*))
+		   *tetramino-board*))
     (#\> (notevery #'(lambda (row) (eql #\. (car (last row))))
-		 *tetramino-board*))
+		   *tetramino-board*))
     (#\v (notevery #'(lambda (char) (eql #\. char))
 		   (car (last *tetramino-board*))))))
 
 (defun nudge (direction)
-  (unless (collision-p direction)
-    (setf *tetramino-board*
-	  (case direction
-	    (#\< (loop for row in *tetramino-board*
-		    collect (append (cdr row) '(#\.))))
-	    (#\> (loop for row in *tetramino-board*
-		    collect (cons #\. (butlast row))))
-	    (#\v (cons empty-row (butlast *tetramino-board*)))))))
+  (let ((scratch-board 
+	 (case direction
+	   (#\< (loop for row in *tetramino-board*
+		   collect (append (cdr row) '(#\.))))
+	   (#\> (loop for row in *tetramino-board*
+		   collect (cons #\. (butlast row))))
+	   (#\v (cons empty-row (butlast *tetramino-board*))))))
+    (unless (or (collision-p direction) (clash-p scratch-board))
+      (setf *tetramino-board* scratch-board))))
 
 (defun plunge ()
   (loop until (null (nudge #\v))))
@@ -150,23 +172,30 @@
   
 
 (defun set-tetramino (input)
-  (case input
-    (#\I (setf *active-tetramino* i-tetramino))
-    (#\O (setf *active-tetramino* o-tetramino))
-    (#\Z (setf *active-tetramino* z-tetramino))
-    (#\S (setf *active-tetramino* s-tetramino))
-    (#\J (setf *active-tetramino* j-tetramino))
-    (#\L (setf *active-tetramino* l-tetramino))
-    (#\T (setf *active-tetramino* t-tetramino)))
+  (setf *active-tetramino*
+	(copy-list
+	 (case input
+	   (#\I i-tetramino)
+	   (#\O o-tetramino)
+	   (#\Z z-tetramino)
+	   (#\S s-tetramino)
+	   (#\J j-tetramino)
+	   (#\L l-tetramino)
+	   (#\T t-tetramino))))
+  (setf *board* (combine-boards))
   (insert-tetramino))
 
-(defun print-board ()
-  (print-matrix 
-   (loop for ya in *board*
-      for yb in *tetramino-board*
-      collect (loop for xa in ya
-		 for xb in yb
-		 collect (if (eql xa #\.) (char-downcase xb) xa)))))
+(defun combine-boards (&key (active nil))
+  (loop for ya in *board*
+     for yb in *tetramino-board*
+     collect (loop for xa in ya
+		for xb in yb
+		collect (if (blank-p xa)
+			    (if active xb (char-downcase xb))
+			    xa))))
+
+(defun print-board (&key (active nil))
+  (print-matrix (combine-boards :active active)))
 
 (defun display (val)
   (format t "~a~%" val))
